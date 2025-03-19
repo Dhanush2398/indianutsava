f76fe7b06882ab37c9a6f08be4fd3477ea021569
dhanush <dhanush.dhoni007@gmail.com>
2025-03-19 11:03:49 +0530

    change

--- js/main.js
+++ js/main.js
@@ -564,3 +564,18 @@
     });
 });
 
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
